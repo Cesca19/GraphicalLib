0418b75d3a6691417891c0a4a261658e9f7253c6
Francesca Attindehou <francesca.attindehou@epitech.eu>
2024-11-19 22:09:07 +0100

    [Fix] box moving algorithm

--- GraphicalLib/CircleAnimated.cpp
+++ GraphicalLib/CircleAnimated.cpp
@@ -12,11 +12,25 @@ void CircleAnimated::Update()
     Vector2f pos = _circle->GetPosition();
     float radius = _circle->GetRadius();
     
-    if (pos.x - radius <= 0 || pos.x + radius >= _windowWidth)
-        _velocity.x = -_velocity.x;
-    if (pos.y - radius <= 0 || pos.y + radius >= _windowHeight)
-        _velocity.y = -_velocity.y;
     pos.x += _velocity.x;
     pos.y += _velocity.y;
+
+    if (pos.x - radius < 0) {
+        pos.x = 0 + radius;
+        _velocity.x = -_velocity.x;
+    }
+    else if (pos.x + radius > 0 + _windowWidth) {
+        pos.x = 0 + _windowWidth - radius; 
+        _velocity.x = -_velocity.x;
+    }
+
+    if (pos.y - radius < 0) {
+        pos.y = 0 + radius;
+        _velocity.y = -_velocity.y;
+    }
+    else if (pos.y + radius > 0 + _windowHeight) {
+        pos.y = 0 + _windowHeight - radius;
+        _velocity.y = -_velocity.y;
+    }
     _circle->SetPosition(pos);
 }
