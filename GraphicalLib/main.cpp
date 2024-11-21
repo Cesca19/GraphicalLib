21ea114b7e37b54e08aba8ad865fb4ad040f9a53
Vy Vu <74190308+bellavee@users.noreply.github.com>
2024-11-21 10:58:46 +0100

    update compilation with args

--- GraphicalLib/main.cpp
+++ GraphicalLib/main.cpp
@@ -1,14 +1,25 @@
 #include "AppCircles.h"
 #include "AppBrickBreaker.h"
 
-int main(int argc, char** args) 
+int main(int argc, char** argv) 
 {
 	//AppCircles app(SDL);
 	//app.Init(1200, 800, "Circle App", 10);
 	//app.Run();
 
-	AppBrickBreaker appBrick(SDL);
+	DisplayMode dm = RAYLIB;
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "-sdl") == 0) {
+			dm = SDL;
+		}
+		if (strcmp(argv[i], "-raylib") == 0) {
+			dm = RAYLIB;
+		}
+	}
+
+	AppBrickBreaker appBrick(dm);
 	appBrick.Init(1200, 800, "Brick Breaker");
 	appBrick.Run();
 	return 0;
 }
+
